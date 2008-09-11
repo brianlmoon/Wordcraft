5322931ace7e7aaf93742d5120ed300b7e6c1dff
Brian Moon <brian@moonspot.net>
2008-09-11 04:46:41 +0000

    nice, simple, Basic template

---
+++ templates/Basic/footer.php
@@ -0,0 +1,51 @@
+            <!-- primary content end -->
+
+        </div>
+
+        <div id="secondarycontent" onclick="alert(document.getElementById('secondarycontent').clientHeight);">
+
+            <!-- secondary content start -->
+
+            <h3>Tags</h3>
+            <div class="content">
+                <ul class="linklist">
+                    <?php foreach($WCDATA["tags"] as $tag) { ?>
+                        <li><a href="<?php echo $tag["url"]; ?>"><?php echo $tag["tag"]; ?> (<?php echo $tag["post_count"]; ?>)</a></li>
+                    <?php } ?>
+                </ul>
+            </div>
+
+            <?php if(isset($WCDATA["admin"])) { ?>
+            <h3>Admin</h3>
+            <div class="content">
+                <ul class="linklist">
+                    <li><a href="<?php echo $WCDATA["admin"]["base_url"]; ?>">Dashboard</a></li>
+                    <li><a href="<?php echo $WCDATA["admin"]["logout_url"]; ?>">Log Out</a></li>
+                    <li><a href="<?php echo $WCDATA["admin"]["new_post_url"]; ?>">New Post</a></li>
+                    <li><a href="<?php echo $WCDATA["admin"]["new_page_url"]; ?>">New Page</a></li>
+                    <?php if(isset($WCDATA["admin"]["edit_post_url"])) { ?>
+                        <li><a href="<?php echo $WCDATA["admin"]["edit_post_url"]; ?>">Edit Post</a></li>
+                    <?php } ?>
+                    <?php if(isset($WCDATA["admin"]["edit_page_url"])) { ?>
+                        <li><a href="<?php echo $WCDATA["admin"]["edit_page_url"]; ?>">Edit Page</a></li>
+                    <?php } ?>
+                </ul>
+            </div>
+            <?php  } ?>
+
+            <!-- secondary content end -->
+
+        </div>
+
+        <div id="footer">
+
+        Powered by <a href="http://code.google.com/p/wordcraft/">Wordcraft</a>.
+
+        </div>
+
+    </div>
+
+</div>
+
+</body>
+</html>
