ade52a6ea138f7826e3abd3fc3d64c994f86d14a
Brian Moon <brian@moonspot.net>
2009-02-28 05:52:30 +0000

    Set the proper doctype and character set

--- admin/login.php
+++ admin/login.php
@@ -53,9 +53,10 @@ if(count($_POST)){
 
 }
 
+header("Content-Type: text/html; charset=UTF-8");
+
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" >
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <head>
     <title>Wordcraft Login</title>
 
